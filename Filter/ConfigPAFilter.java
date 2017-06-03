78d31308654311ef366a4f005a9d761bc5e18cef
vijayAnnadurai <vijayannadurai@gmail.com>
2017-06-02 23:08:38 -0400

    updated configuration file

--- Filter/ConfigPAFilter.java
+++ Filter/ConfigPAFilter.java
@@ -10,17 +10,20 @@ package filter;
  */
 public class ConfigPAFilter {
    //Kick Off limit variable
-	public static Integer MAX_KICK_OFF = 20;
+	public static Integer MAX_KICK_OFF = 1000;
 	//total number of buckets. 0 ... 1999 = >total = 2000.
-	public static Integer BUCKET_SIZE = 1999;
+	public static Integer BUCKET_SIZE = 16383;
 	//Hash algorithm to choose. "MD5" or "SHA1".
-	public static String HASH_ALGORITHM = "MD5";
+	public static String HASH_ALGORITHM = "SHA1";
 	
 	//input data file.
-	public static String Input_FileName = "data/Insert6500Num.txt";
+	public static String Input_FileName = "data/OneMillionInput.txt";
+	
+	
 	
 	//lookup file
 	public static String LookUp_FileName = "data/OneMillionLookup.txt";
+	
 	
 	//variable to test both standard cuckoo filter and Position Aware Cuckoo Filter.
 	//if it is TRUE, then PA filter else, standard filter.
@@ -28,3 +31,4 @@ public class ConfigPAFilter {
 	
 	
 }
+
